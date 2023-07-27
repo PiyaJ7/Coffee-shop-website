919deb49c292422d15ae124dd5e7b3d6e6ade78f
E M P J De Saram <jayodya7desaram@gmail.com>
2023-07-27 19:19:41 +0530

    Design header section
    Design header section using HTML/ CSS and JavaScript.

---
+++ script.js
@@ -0,0 +1,17 @@
+let mybutton = document.getElementById("myBtn");const 
+
+textAnimation = document.getElementById("home-link");
+let animationDuration = 4;
+
+function startAnimation() {
+    textAnimation.style.opacity = 1;
+    textAnimation.style.transition = `opacity ${animationDuration}s linear`;
+
+    setTimeout(function() {
+        textAnimation.style.opacity = 0;
+    }, animationDuration * 1000);
+}
+
+startAnimation();
+
+setInterval(startAnimation, animationDuration * 1000 * 2);
