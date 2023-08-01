51e336200a51c2ea5e65478200b8525d2e4bfb20
E M P J De Saram <jayodya7desaram@gmail.com>
2023-08-02 00:46:07 +0530

    Finish creating the website
    Add scrollTop property using JavaScript.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-let mybutton = document.getElementById("myBtn"); const
+let mybutton = document.getElementById("myBtn");
 
-    textAnimation = document.getElementById("home-link");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+const textAnimation = document.getElementById("home-link");
 let animationDuration = 4;
 
 function startAnimation() {
     textAnimation.style.opacity = 1;
     textAnimation.style.transition = `opacity ${animationDuration}s linear`;
 
-    setTimeout(function () {
+    setTimeout(function() {
         textAnimation.style.opacity = 0;
     }, animationDuration * 1000);
 }
